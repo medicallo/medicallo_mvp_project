c3a9289d4a7d0c4b74b07cda1b26003be958b660
mlayeh94 <mlayehmedamine@gmail.com>
2022-11-17 16:00:18 +0100

    adding images

---
+++ flask_app/controllers/doctors.py
@@ -0,0 +1,34 @@
+from flask import Flask, redirect, render_template, session, request,flash
+from flask_app import app
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask_app.models.doctor import Doctor
+
+@app.route('/admin/dashboard')
+def doc():
+    doctors=Doctor.get_all()
+    
+    return render_template('admin_dashboard.html',doctors=doctors)
+
+
+@app.route('doctor',methods=['POST'])
+def doctor():
+    Doctor.create_doctor(request.form)
+    print('*'*20,Doctor.create_doctor(request.form))
+    return redirect('/admin/dashboard')
+
+
+@app.route('/doctor/edit', methods=['POST'] )
+def doctor_edit():
+    Doctor.update(request.form)
+    print('*'*20,Doctor.update(request.form))
+    return redirect('/admin/dashboard')
+
+
+@app.route('/doctor/delete', methods=['POST'] )
+def doctor_edit():
+    Doctor.destroy(request.form)
+    print('*'*20,Doctor.destroy(request.form))
+    return redirect('/admin/dashboard')
+
+
