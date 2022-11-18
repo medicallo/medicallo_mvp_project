fc99a503e9013420bd4bc40073d029e5c42c02c3
AtharBaccouche <baccouche.athar@gmail.com>
2022-11-18 08:18:01 +0100

    login and register

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,10 +1,10 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app import app
-# from flask import flash
-# import re
-# from flask_bcrypt import Bcrypt
-# bcrypt = Bcrypt(app) 
-# EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+from flask import flash
+import re
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app) 
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 class User:
     db_name ="doc"
@@ -17,7 +17,7 @@ class User:
         self.created_at=data['created_at']
         self.updated_at=data['updated_at']
         self.admin_id=data['admin_id']
-        self.users = 0
+
     @classmethod
     def create_user(cls,data):
         query = "INSERT INTO users (first_name,last_name,email,password) VALUES (%(first_name)s,%(last_name)s,%(email)s, %(password)s);"
@@ -47,47 +47,26 @@ class User:
         return users
 
     @staticmethod
-    def validate_user(data):
+    def validate_register(data):
         is_valid = True # we assume this is true.
         query="SELECT * FROM users WHERE email = %(email)s ;"
         result = connectToMySQL(User.db_name).query_db(query,data)
         if len(result)>=1:
-            flash("Email Already Exist", 'email')
+            flash("Email Already Exist")
         if not EMAIL_REGEX.match(data['email']): 
             flash("Incorrect Email", 'email')
             is_valid = False
-        if len(data['first_name']) < 2:
-            flash("first Name must be at least 2 characters.",'email' )
+        if len(data['first_name']) < 3:
+            flash("first Name must be at least 3 characters.")
             is_valid = False
-        if len(data['last_name']) < 2:
-            flash("last name must be at least 2 characters.", 'email')
+        if len(data['last_name']) < 3:
+            flash("last name must be at least 3 characters.")
             is_valid = False
         if len(data['password']) < 3:
-            flash("password must be at least 8 characters.", 'email')
+            flash("password must be at least 8 characters.")
             is_valid = False
         if data['password']!=data ['confirm_password'] :
-            flash("password Don't match.", 'email')
+            flash("password Don't match.")
             is_valid = False
         return is_valid
-
-
-    @classmethod
-    def get_user_with_show( cls , data ):
-        query ="SELECT * FROM users LEFT JOIN shows ON shows.user_id = users.id WHERE users.id = %(id)s;"
-        results = connectToMySQL(cls.db_name).query_db( query , data )
-        # results will be a list of user objects with the burger attached to each row. 
-        user = cls( results[0] )
-        for row_from_db in results:
-            # Now we parse the burger data to make instances of shows and add them into our list.
-            show_data = {
-                "id" : row_from_db["shows.id"],
-                "title" : row_from_db["shows.title"],
-                "network" : row_from_db['network'],
-                "description" : row_from_db['description'],
-                "date" : row_from_db['date'],
-                "created_at" : row_from_db["shows.created_at"],
-                "updated_at" : row_from_db["shows.updated_at"]
-            }
-            user.show.append( Show( show_data ) )
-        return user
     
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask_app import app
-from flask_app.controllers import hospitals, users
+from flask_app.controllers import users 
 
 
 
-if __name__=='__main__':
-    app.run(debug=True)  
+if __name__=="__main__":
+    app.run(debug=True,port=5100)  
